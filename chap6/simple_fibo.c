5fb371f47f5b1bc2a9aeca8b1197b7d8a76ad101
UltraViolet33 <ulysse.valdenairepro@gmail.com>
2022-09-19 10:40:02 +0200

    start chap 7

---
+++ chap6/simple_fibo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int value1 = 0;
+    int value2 = 1;
+    int current = 1;
+
+    while (current <= 500)
+    {
+        value1 = current;
+        printf("%i\n", value2);
+        printf("%i\n", current);
+        current = current + value2;
+        value2 = current;
+        current = current + value1;   
+    }
+
+    return 0;
+}
---
+++ chap7/minimum.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+int minimum (int values[10])
+{
+    int min_value, i;
+
+    min_value = values[0];
+
+    for (i = 1; i < 10; i++)
+    {
+        if (values[i] < min_value)
+        {
+            min_value = values[i];
+        }
+    }
+
+    return min_value;
+}
+
+
+int main (void)
+{
+
+    int scores[10], i, min_score;
+    int minimum (int values[10]);
+
+    printf("Enter 10 scores\n");
+
+    for (i = 0; i < 10; i++)
+    {
+        scanf("%i", &scores[i]);
+    }
+
+    min_score = minimum(scores);
+    printf("\nMinimum score is %i\n", min_score);
+
+    return 0;
+}
