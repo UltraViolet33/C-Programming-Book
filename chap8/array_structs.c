f18357a6bcbdc2f3650f2c167280907a7ac7040b
UltraViolet33 <ulysse.valdenairepro@gmail.com>
2022-09-21 12:49:31 +0200

    start chap 9

---
+++ chap8/array_structs.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+struct time
+{
+    int hour;
+    int minutes;
+    int seconds;
+};
+
+int main (void)
+{
+    struct time time_update (struct time now);
+    struct time times_test[5] =  { {11, 59, 59}, {12, 45, 15}, {15, 21, 14}, {17, 12, 14}, {12, 23, 21} };
+
+    int i;
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("Time : %.2i:%.2i:%.2i\n", times_test[i].hour, times_test[i].minutes, times_test[i].seconds);
+
+        times_test[i] = time_update(times_test[i]);
+
+        printf("Time on second later... : %.2i:%.2i:%.2i\n", times_test[i].hour, times_test[i].minutes, times_test[i].seconds);
+
+    }
+
+
+    return 0;
+
+}
+
+
+
+struct time time_update (struct time now)
+{
+    now.seconds++;
+
+    if (now.seconds == 60)
+    {
+        now.seconds = 0;
+        now.minutes++;
+
+        if (now.minutes == 60)
+        {
+            now.minutes = 0;
+            now.hour++;
+        }
+
+        if (now.hour == 24)
+        {
+            now.hour = 0;
+        }
+    }
+
+    return now;
+}
---
+++ chap9/concat.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+
+void concat (char result[], const char str1[], int n1, const char str2[], int n2)
+{
+    int i, j;
+
+    for (i = 0; i < n1; i++)
+    {
+        result[i] = str1[i];
+    }
+
+    for (j = 0; j < n2; j++)
+    {
+        result[n1 + j] = str2[j];
+    }
+}
+
+
+int main (void)
+{
+    void concat (char result[], const char str1[], int n1, const char str2[], int n2);
+
+    const char s1[6] = {'H', 'e', 'l', 'l', 'o', ' '};
+    const char s2[6] = {'W', 'o', 'r', 'l', 'd', '!'};
+    char s3[12];
+
+    int i;
+
+    concat(s3, s1, 6, s2, 6);
+
+    for (i = 0; i < 12; i++)
+    {
+        printf("%c", s3[i]);
+    }
+
+    printf("\n");
+
+    return 0;   
+}
